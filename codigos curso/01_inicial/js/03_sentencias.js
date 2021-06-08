9e2096c279ab0b1af7a060204e538a7e7e581fc5
Develop and System <developandsystem@gmail.com>
2021-06-08 13:01:16 +0200

    Update 03_sentencias.js

--- codigos curso/01_inicial/js/03_sentencias.js
+++ codigos curso/01_inicial/js/03_sentencias.js
@@ -26,15 +26,73 @@ if (nota < 0 || nota > 10) {
 // nota > 5 ? alert("mayor que 5") : alert("suspenso");
 
 // pedir un numero por teclado
-// generar un numero aleatorio 
+// generar un numero aleatorio
 
-// continua pidiento numeros por prompt hasta que acierte el numero 
+// continua pidiento numeros por prompt hasta que acierte el numero
 // generado.
 // cuando se acierte aparecerá un alert diciendo el número de intentos
 // utilizado
-nota = 6
+/*nota = 6
 while (nota<10) {
     alert(`nota es ${nota}`)
     nota++;
+}*/
+
+/*
+let aleatorio = Math.round(Math.random() * 11);
+let intentos=1;
+let numero = Number(prompt('Introduce numero'));
+
+
+while (numero != aleatorio) {
+    intentos++
+    numero = Number(prompt('Introduce numero'));   
 }
+
+alert(`Numero acertado\nIntentos utilizados ${intentos}`)*/
+
+/*do {
+    
+} while (condition);*/
+
+//console.log("El aleatorio generado es: " + aleatorio);
+
+let coleccionPalabras = [
+  "uno",
+  "dos",
+  "tres",
+  "cuatro",
+  "cinco",
+  123,
+  123,
+  true,
+];
+/*for (let index = 0; index < coleccionPalabras.length; index++) {
+    console.log(coleccionPalabras[index]);
+    console.log(`\t${typeof coleccionPalabras[index]}`);
+    //if (typeof coleccionPalabras[index]== undefined)
+}*/
+
+/*coleccionPalabras.forEach((item, posicion, array) => {
+  console.log(item);
+  console.log(`\t${posicion}`);
+  console.log(`\t ${typeof item}`);
+  console.log(`\t\t ${array}`);
+});*/
+
+// for in -->
+// for of --> el de java. se obtiene el elemento de cada una de las posiciones
+
+for (const key in coleccionPalabras) {
+  //const element = object[key];
+  if (key % 2 == 0) {
+    console.log(`${key}`);
+    console.log(`\t ${coleccionPalabras[key]}`);
+  }
+}
+
+/*for (const iterator of coleccionPalabras) {
+    console.log(iterator);
+}*/
+
 
