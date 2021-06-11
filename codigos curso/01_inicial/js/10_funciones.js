92b8913b3e06321446430ccc1ed4da80889c4a2e
Develop and System <developandsystem@gmail.com>
2021-06-11 12:45:44 +0200

    update

--- codigos curso/01_inicial/js/10_funciones.js
+++ codigos curso/01_inicial/js/10_funciones.js
@@ -1,6 +1,6 @@
 // funtion nombre (parametros){
 //}
-
+/*
 function funcionNormal() {
   console.log("Ejecucion normal");
 }
@@ -41,4 +41,50 @@ funcionParametros("Hola", 5);
 funcionParametrosAdicionales();
 funcionParametrosDefecto("cosa pasada");
 funcionSuma(4, 6);
-console.log(funcionDivision(4, 2));
+console.log(funcionDivision(4, 2));*/
+
+function mostrarMensaje(parametro) {
+  console.log("Esto es una funcion que muestra un mensaje de " + parametro);
+}
+
+function funcionCompleta(funcion, nombre) {
+  console.log("Ejecucion de la funcion de forma completa");
+  funcion(nombre);
+}
+
+function funcionConRetorno() {
+  return "Ejemplo de retorno";
+}
+
+function funcionRetornoFuncion(params) {
+  return (params) => {
+    console.log("ejecucion de algo");
+  };
+}
+
+console.log(funcionConRetorno());
+
+//funcionCompleta(mostrarMensaje, "Borja");
+funcionCompleta((nombre) => {
+  console.log("El usuario " + nombre + " no puede hacer cosas");
+}, "borja");
+
+funcionCompleta((nombre) => {
+  console.log("Esto es de " + nombre);
+}, "Pepe");
+
+//console.log(mostrarMensaje);
+
+function asincroniaInterval() {
+  setInterval(() => {
+    console.log("funcion interval");
+  }, 1000);
+}
+
+function asicroniaTimeOut() {
+  setTimeout(() => {
+    console.log("funcion timeout");
+  }, 3000);
+}
+//asincroniaInterval();
+asicroniaTimeOut();
