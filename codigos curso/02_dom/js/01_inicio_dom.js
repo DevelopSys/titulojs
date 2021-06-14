9dc43438b40927d7872544d5d5f6c47a485e2772
Develop and System <developandsystem@gmail.com>
2021-06-14 10:53:45 +0200

    update

---
+++ codigos curso/02_dom/js/01_inicio_dom.js
@@ -0,0 +1,8 @@
+console.log('Inicio busquedas js');
+document.get('boton_buscar').disabled = false
+document.querySelector('#zcxzxc .casdasd[name=asdasdad]').
+//console.log(nodoPrimario);
+if (new Date().getHours > 18){
+    document.getElementById('boton_buscar').disabled = false
+}
+console.log(document);
