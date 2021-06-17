0adab5af1d9cffb736dceb647c457e7432d5e97c
Develop and System <developandsystem@gmail.com>
2021-06-17 09:05:07 +0200

    update

--- codigos curso/02_dom/js/01_inicio_dom.js
+++ codigos curso/02_dom/js/01_inicio_dom.js
@@ -12,10 +12,27 @@ let elemento = document.querySelector(".form-control");
     console.log(element.value);
 });*/
 
-let buttonForm = document.querySelector("button");
+let buttonForm = document.querySelector("#botonMostrar");
+let buttonGuardar = document.querySelector("#botonGuardar");
 buttonForm.addEventListener("click", (evento) => {
   //console.log(evento.target);
-  let nombre = document.querySelector("#exampleInputNombre1");
+  let nombre = document.querySelector("#exampleInputNombre1").value;
+  let apellido = document.querySelector("#exampleInputApellido1").value;
+  let edad = document.querySelector("#exampleInputEdad1").value;
+  let profesionSelect = document.querySelector("select");
+  let sexo = document.querySelector("input[name='sexo_grupo']:checked").value;
+
+  if (nombre.length > 0 && apellido.length > 0) {
+    document.querySelector("#nombrePerfil").textContent = nombre;
+    document.querySelector("#apellidoPerfil").textContent = apellido;
+    document.querySelector("#edadPerfil").textContent = edad;
+    document.querySelector("#profesionPerfil").textContent =
+      profesionSelect.options[profesionSelect.selectedIndex].value;
+    document.querySelector("#sexoPerfil").textContent = sexo;
+  } else {
+    alert('Faltan datos')
+  }
+  /*
   let apellido = document.querySelector("#exampleInputApellido1");
   let edad = document.querySelector("#exampleInputEdad1");
   if (nombre.value.length > 0 && apellido.value.length > 0) {
@@ -24,14 +41,29 @@ buttonForm.addEventListener("click", (evento) => {
     );
   } else {
     alert("falta algun dato");
-  }
+  }*/
+
+  /* let check = document.querySelector("input[type='checkbox']");
+  let select = document.querySelector("select");
+  let grupo = document.querySelector("input[name='sexo_grupo']:checked");
+
+  //console.log(select.selectedOptions[0]);
+  console.log(grupo);
+  console.log(select.options[select.selectedIndex].value);
+  console.log(check.checked);
+  console.log(grupo.value); */
 });
 
-document
-  .querySelector("#exampleInputNombre1")
-  .addEventListener("keypress", (event) => {
-    console.log(event);
-  });
+buttonGuardar.addEventListener('click',()=>{
+  //console.log('pulsado guardar');
+  let nombre = document.querySelector("#exampleInputNombre1").value;
+  let contenidoUsuarios = document.querySelector('#contenidoUsuarios');
+  contenidoUsuarios.innerHTML = `${contenidoUsuarios.innerHTML} <h3>${nombre}</h3>`
+  //console.log(contenidoUsuarios);
+  
+})
+
+
 
 let palabra = "";
 /*function inputTeclaPulsada(nombre) {
@@ -50,3 +82,22 @@ let palabra = "";
         alert('falta algun dato')
     }
 }*/
+
+/*function metodoCambioSelect() {
+  let select = document.querySelector("select");
+  console.log(select.options[select.selectedIndex].value);
+}*/
+
+document.querySelector("select").addEventListener("change", (event) => {
+  console.log(event.target.options[event.target.selectedIndex].value);
+});
+
+document
+  .querySelector("input[type='checkbox']")
+  .addEventListener("change", (event) => {
+    //console.log(event.target.checked);
+    let seleccion = event.target.checked;
+    if (!seleccion) {
+      document.querySelector("#exampleInputNombre1").innerHTML = " ";
+    }
+  });
